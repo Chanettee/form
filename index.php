f22e2981ab16a2f7a07c2567486e2d7ae5ff4a8b
Chanettee Leklee <86229643+Chanettee@users.noreply.github.com>
2021-12-16 17:11:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <title>63116503</title>
 
     <style>
@@ -40,39 +42,43 @@
             <br>
             <form action="">
                 <div class="form-group">
-                    <label for="frmCard"> เลขบัตรประชาชน* : </label>
-                    <input type="text" id="frmCard" name="card" placeholder="x-xxxx-xxxxx-x-xx"
-                        onkeyup="autoTab(this,1)" required /><br />
+
+                    <div class="col-md-4">
+                        <label for="IDCard"> เลขบัตรประชาชน* : </label>
+                        <input type="text" id="IDCard" name="card" class="form-control" placeholder="x-xxxx-xxxxx-x-xx"
+                            onkeyup="autoTab(this,1)" required /><br />
+                    </div>
+
 
                     <br />
-                    <label for="frmpre">คำนำหน้านาม* : </label>
-                    <select class="form-inline" id="frmpre" required>
+                    <label for="prefix">คำนำหน้านาม* : </label>
+                    <select class="form-inline" id="prefix" required>
                         <option value="" selected>--เลือก--</option>
-                        <option value="1">นาย</option>
-                        <option value="2">นาง</option>
-                        <option value="3">นางสาว</option>
+                        <option value=">นาย">นาย</option>
+                        <option value="นาง">นาง</option>
+                        <option value="นางสาว">นางสาว</option>
                     </select>
 
-                    <input type="text" id="frmName" class="form-inline" name="Fname" placeholder="ชื่อ" required />
+                    <input type="text" id="Fname" class="form-inline" name="Fname" placeholder="ชื่อ" required />
 
-                    <input type="text" id="frmLast" class="form-inline" name="Lname" placeholder="นามสกุล"
+                    <input type="text" id="Lname" class="form-inline" name="Lname" placeholder="นามสกุล"
                         required /><br />
 
                     <br />
-                    <label for="frmSex"> เพศ* : </label>
-                    <input type="radio" name="gender" value=""> ชาย
-                    <input type="radio" name="gender" value=""> หญิง
-                    <input type="radio" name="gender" value=""> อื่นๆ <br />
+                    <label for="Sex"  class="form-inline" id="sex"> เพศ* : </label>
+                    <input type="radio" name="gender" value="ชาย"> ชาย
+                    <input type="radio" name="gender" value="หญิง"> หญิง
+                    <input type="radio" name="gender" value="อื่นๆ"> อื่นๆ <br />
 
                     <br />
 
                     <div class="col-md-3 mb-1">
                         <label for="frmrel">ศาสนา* : </label>
-                        <select class="custom-select" id="frmrel" required>
+                        <select class="form-control" id="religion" required>
                             <option value="" selected>---เลือกศาสนา--</option>
-                            <option value="1">พุทธ</option>
-                            <option value="2">อิสลาม</option>
-                            <option value="3">คริสต์</option>
+                            <option value="พุทธ">พุทธ</option>
+                            <option value="อิสลาม">อิสลาม</option>
+                            <option value="คริสต์">คริสต์</option>
                         </select>
                     </div>
 
@@ -83,7 +89,7 @@
                             <label for="frmAdd"> ที่อยู่* : </label>
 
                             <div class="col-md-3">
-                                <input type="number" id="number" class="form-control" placeholder="บ้านเลขที่" required />
+                                <input type="text" id="number" class="form-control" placeholder="บ้านเลขที่" required />
                             </div>
 
                             <div class="col-md-3">
@@ -91,22 +97,23 @@
                             </div>
 
                             <div class="col-md-3">
-                                <input type="text" id="road" class="form-control" placeholder="ตำบล/แขวง" required />
+                                <input type="text" id="road" class="form-control" placeholder="ถนน" required />
                             </div>
 
                             <div class="col-md-3">
-                                <input type="text" id="district" class="form-control" placeholder="ตำบล/แขวง" required />
+                                <input type="text" id="district" class="form-control" placeholder="ตำบล/แขวง"
+                                    required />
                             </div>
 
                             <div class="col-md-3 mb-3">
-                                <br/>
+                                <br />
                                 <input type="text" id="Sdistrict" class="form-control" placeholder="อำเภอ/เขต " require>
                             </div>
 
                             <div class="col-md-3 mb-3">
-                                <br/>
-                                <select class="form-control" id="frmprov" required>
-                                    <option  value="" selected>-----เลือกจังหวัด------</option>
+                                <br />
+                                <select class="form-control" id="prov" required>
+                                    <option value="" selected>----เลือกจังหวัด----</option>
                                     <option value="นครศรีธรรมราช">นครศรีธรรมราช</option>
                                     <option value="ตรัง">ตรัง</option>
                                     <option value="สงขลา">สงขลา</option>
@@ -116,7 +123,7 @@
                             </div>
 
                             <div class="col-md-3 mb-3">
-                                <br/>
+                                <br />
                                 <input type="text" id="zip" class="form-control" placeholder="รหัสไปรษณีย์" required />
                             </div> <br />
 
@@ -128,19 +135,21 @@
 
                         <div class="col-md-4 mb-2">
                             <label for=""> โทรศัพท์บ้าน : </label>
-                            <input type="text" class="form-control" id="phone" placeholder="xxx-xxx-xxxx" onkeyup="autoTab2(this,2)" />
+                            <input type="text" class="form-control" id="phone" placeholder="xxx-xxx-xxxx"
+                                onkeyup="autoTab2(this,2)" />
                         </div>
 
                         <div class="col-md-4 mb-2">
                             <label for=""> โทรศัพท์มือถือ* : </label>
-                            <input type="text" class="form-control" id="tel" placeholder="xx-xxx-xxxx" onkeyup="autoTab2(this,2)" required />
+                            <input type="text" class="form-control" id="tel" placeholder="xx-xxx-xxxx"
+                                onkeyup="autoTab2(this,2)" required />
                         </div>
 
                     </div>
 
                     <div class="col-sm-6">
                         <br />
-                        <label for="frmCours" class="checkbox" required> หลักสูตรที่สนใจ* : </label> <br />
+                        <label for="frmCours" class="checkbox" id="branch" required> หลักสูตรที่สนใจ* : </label> <br />
                         <input type="checkbox" name="cours" value="IMI"> IMI (นวัตกรรมสารสนเทศทางการแพทย์) <br />
                         <input type="checkbox" name="cours" value="ITD"> ITD (เทคโนโลยีสารสนเทศและนวัตกรรมดิจิทัล)<br />
                         <input type="checkbox" name="cours" value="MTA"> MTA (เทคโนโลยีมัลติมีเดีย แอนิเมชัน
@@ -150,8 +159,8 @@
                     </div>
 
                     <br />
-                    <div class="bg2">
-                        <button class="btn btn-primary" type="submit" id="btnSubmit">Submit </button>
+                    <div class="d-grid gap-2 col-6 mx-auto" style="margin-bottom:20px">
+                        <button type="submit" class="btn btn-primary" id="btnSubmit">Submit</button>
                     </div>
                 </div>
             </form>
@@ -160,18 +169,26 @@
 </body>
 
 <script type="text/javascript">
-    function btnSubmit_click() {
-        var idnum = $("#frmCard").val();
-        
-        var data = "{";
-        data += '"idnum":"' + $("#frmCard").val() + '"';
-        
-        data += "}";     
-        console.log(JSON.parse(data));
-    }
 
-    $(() => {
-        $("#btnSubmit").click(btnSubmit_click);
+$("#btnSubmit").click(() => {
+        var data = $.parseJSON(`{
+            "IDCard": "${$("#IDCard").val()}", 
+            "prefix": "${$("#prefix").val()}", 
+            "Fname": "${$("#Fname").val()}", 
+            "Lname": "${$("#Lname").val()}",
+            "sex": "${$("#sex").val()}",
+            "religion": "${$("#religion").val()}",
+            "number": "${$("#number").val()}",
+            "moo": "${$("#moo").val()}",
+            "road": "${$("#road").val()}",
+            "district": "${$("#district").val()}",
+            "Sdistrict": "${$("#Sdistrict").val()}",
+            "prov": "${$("#prov").val()}",
+            "zip": "${$("#zip").val()}",
+            "phone": "${$("#phone").val()}",
+            "tel": "${$("#tel").val()}",
+            "branch": "${$("#branch").val()}"}`);
+        console.log(data);
     });
 
     function autoTab(obj) {
